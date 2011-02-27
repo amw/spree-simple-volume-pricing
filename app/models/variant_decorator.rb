42593f50c07ed3d7100256cebbc2442214767215
Adam Wróbel <adam@fluxinc.ca>
2011-02-27 19:39:44 +0100

    Copy master volume prices to new variants.

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -7,6 +7,8 @@ Variant.class_eval do
     :reject_if => :blank_volume_price,
     :allow_destroy => true
 
+  after_create :copy_master_volume_prices
+
   # calculates the price based on quantity
   def volume_price quantity, order = nil
     quantity += order.variant_starting_quantity self if order
@@ -21,5 +23,13 @@ Variant.class_eval do
 
   def blank_volume_price attributes
     attributes['starting_quantity'].blank? && attributes['price'].blank?
+  end
+
+  private
+  def copy_master_volume_prices
+    return if self.is_master?
+    self.volume_prices = self.product.master.volume_prices.map do |vp|
+      VolumePrice.new vp.attributes.slice('starting_quantity', 'price')
+    end
   end
 end
