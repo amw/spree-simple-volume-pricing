cded7f282034596e1514c02713309d3802344390
Adam Wróbel <adam@fluxinc.ca>
2011-02-27 17:01:22 +0100

    Copy master volume prices when cloning product.

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -5,4 +5,11 @@ Product.class_eval do
     return unless master && (master.changed? || master.new_record? || master.changed_for_autosave?)
     raise ActiveRecord::Rollback unless master.save
   end
+
+  private
+  def duplicate_extra original
+    self.master.volume_prices = original.master.volume_prices.map do |vp|
+      VolumePrice.new vp.attributes.slice('starting_quantity', 'price')
+    end
+  end
 end
