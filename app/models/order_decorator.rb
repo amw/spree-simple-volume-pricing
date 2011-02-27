b0d93b573c7257a3bee6ed1c60f3db0177ca39be
Adam Wróbel <adam@fluxinc.ca>
2011-02-27 12:22:19 +0100

    Update line items prices when user logs in.

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -1,4 +1,12 @@
 Order.class_eval do
+  def add_variant_with_volume_prices variant, quantity = 1
+    current_item = add_variant_without_volume_prices variant, quantity
+    current_item.price = variant.volume_price current_item.quantity, self
+    raise ActiveRecord::RollBack unless current_item.save
+    update!
+  end
+  alias_method_chain :add_variant, :volume_prices
+
   # By default volume price is calculated based only on quantity of the current
   # order. If you want to have "Volume Customers" - people who purchase at
   # volume prices, but making multiple orders of smaller quantities you can
@@ -10,11 +18,15 @@ Order.class_eval do
     0
   end
 
-  def add_variant_with_volume_prices variant, quantity = 1
-    current_item = add_variant_without_volume_prices variant, quantity
-    current_item.price = variant.volume_price current_item.quantity, self
-    raise ActiveRecord::RollBack unless current_item.save
-    update!
+  # This is required for Volume Customers
+  # It updates line items prices when user logs in
+  def recalculate_prices_on_user_association
+    if user_id_changed? || email_changed?
+      line_items.each do |li|
+        li.price = li.variant.volume_price li.quantity, self
+        li.save
+      end
+    end
   end
-  alias_method_chain :add_variant, :volume_prices
+  before_save :recalculate_prices_on_user_association
 end
