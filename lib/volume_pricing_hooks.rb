e7716e18e0cc1d556a20b447357c28fb6c30b677
Adam Wróbel <adam@fluxinc.ca>
2011-02-28 13:47:04 +0100

    List master variant volume prices in the cart form

--- lib/volume_pricing_hooks.rb
+++ lib/volume_pricing_hooks.rb
@@ -1,4 +1,5 @@
 class VolumePricingHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_product_tabs, :partial => "admin/shared/vp_product_tab"
   insert_after :admin_variant_edit_form, :partial => "admin/variants/volume_prices"
+  replace :product_price, :partial => "products/volume_prices"
 end
